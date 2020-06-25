a7b74412a8cd200b369b980c94dd6c299f2bce1a
AhmedSamirOuf <samir@devsquads.com>
2020-06-25 12:07:23 +0200

    expose docker port to local machine

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 echo "waiting for database to start..."
 /wait.sh
 echo "setting up database..."
-bundle exec rake db:migrate 2>/dev/null || bundle exec rake db:create db:migrate RAILS_ENV=test
+bundle exec rake db:migrate 2>/dev/null || bundle exec rake db:create db:migrate
 echo "starting application server..."
 rm -f tmp/pids/server.pid
 bin/rails server -b 0.0.0.0 -p "$APPLICATION_PORT"
